67ee26c63d89214743578aba111b7ac133c5fcae
shaman <KoLo1212>
2021-09-16 22:29:06 +0300

    Add db.properties

--- src/main/java/edu/shmonin/university/SpringConfig.java
+++ src/main/java/edu/shmonin/university/SpringConfig.java
@@ -1,8 +1,10 @@
 package edu.shmonin.university;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -10,20 +12,30 @@ import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("edu.shmonin.university")
+@PropertySource("classpath:db.properties")
 public class SpringConfig {
+
+    @Value("${db.driver}")
+    private String dbDriverClass;
+    @Value("${db.sourceName}")
+    private String dbUrl;
+    @Value("${db.user}")
+    private String dbUser;
+    @Value("${db.password}")
+    private String dbPassword;
 
     public DataSource dataSource() {
         var dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/university");
-        dataSource.setUsername("user");
-        dataSource.setPassword("123");
+        dataSource.setDriverClassName(dbDriverClass);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUser);
+        dataSource.setPassword(dbPassword);
 
         return dataSource;
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate(){
+    public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
     }
 }
