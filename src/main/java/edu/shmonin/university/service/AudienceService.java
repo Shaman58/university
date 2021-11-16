dfa0fe7805bf7bae11b276d6da799d3a0ae3da8e
shaman <KoLo1212>
2021-11-16 16:00:14 +0300

    add EntityException and Logger

--- src/main/java/edu/shmonin/university/service/AudienceService.java
+++ src/main/java/edu/shmonin/university/service/AudienceService.java
@@ -3,10 +3,13 @@ package edu.shmonin.university.service;
 import edu.shmonin.university.dao.AudienceDao;
 import edu.shmonin.university.dao.LectureDao;
 import edu.shmonin.university.model.Audience;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class AudienceService implements EntityService<Audience> {
@@ -19,6 +22,12 @@ public class AudienceService implements EntityService<Audience> {
 
     private final AudienceDao jdbcAudienceDao;
     private final LectureDao jdbcLectureDao;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final String GET_ERROR_MESSAGE = "Audience was not got";
+    private static final String GET_ALL_ERROR_MESSAGE = "Audiences were not got";
+    private static final String CREATE_ERROR_MESSAGE = "Audience was not created";
+    private static final String UPDATE_ERROR_MESSAGE = "Audience was not updated";
+    private static final String DELETE_ERROR_MESSAGE = "Audience was not deleted";
 
     public AudienceService(AudienceDao jdbcAudienceDao, LectureDao jdbcLectureDao) {
         this.jdbcAudienceDao = jdbcAudienceDao;
@@ -27,18 +36,31 @@ public class AudienceService implements EntityService<Audience> {
 
     @Override
     public Audience get(int audienceId) {
-        return jdbcAudienceDao.get(audienceId);
+        try {
+            return jdbcAudienceDao.get(audienceId);
+        } catch (Exception e) {
+            logger.error(GET_ERROR_MESSAGE);
+            throw new EntityException(GET_ERROR_MESSAGE);
+        }
     }
 
     @Override
     public List<Audience> getAll() {
-        return jdbcAudienceDao.getAll();
+        try {
+            return jdbcAudienceDao.getAll();
+        } catch (Exception e) {
+            logger.error(GET_ALL_ERROR_MESSAGE);
+            throw new EntityException(GET_ALL_ERROR_MESSAGE);
+        }
     }
 
     @Override
     public void create(Audience audience) {
         if (validateAudience(audience)) {
             jdbcAudienceDao.create(audience);
+        } else {
+            logger.error(CREATE_ERROR_MESSAGE);
+            throw new EntityException(CREATE_ERROR_MESSAGE);
         }
     }
 
@@ -46,6 +68,9 @@ public class AudienceService implements EntityService<Audience> {
     public void update(Audience audience) {
         if (validateAudience(audience)) {
             jdbcAudienceDao.update(audience);
+        } else {
+            logger.error(UPDATE_ERROR_MESSAGE);
+            throw new EntityException(UPDATE_ERROR_MESSAGE);
         }
     }
 
@@ -53,6 +78,9 @@ public class AudienceService implements EntityService<Audience> {
     public void delete(int audienceId) {
         if (jdbcLectureDao.getByAudienceId(audienceId).isEmpty()) {
             jdbcAudienceDao.delete(audienceId);
+        } else {
+            logger.error(DELETE_ERROR_MESSAGE);
+            throw new EntityException(DELETE_ERROR_MESSAGE);
         }
     }
 
---
+++ src/main/java/edu/shmonin/university/service/EntityException.java
@@ -0,0 +1,7 @@
+package edu.shmonin.university.service;
+
+public class EntityException extends RuntimeException {
+    public EntityException(String message) {
+        super(message);
+    }
+}
