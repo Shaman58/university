dd208e5558755742426c31fe71afca8336e307cc
shaman <KoLo1212>
2022-02-22 18:19:12 +0300

    add ExceptionController

---
+++ src/main/java/edu/shmonin/university/controller/ExceptionController.java
@@ -0,0 +1,19 @@
+package edu.shmonin.university.controller;
+
+import edu.shmonin.university.exception.ServiceException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDate;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(ServiceException.class)
+    public String getException(ServiceException e, Model model) {
+        var message = String.format("%s %s", LocalDate.now(), e.getMessage());
+        model.addAttribute("message", message);
+        return "exceptions/exception";
+    }
+}
